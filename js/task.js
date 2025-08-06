c1d4df1aed768aaf7dc2e959a393b91a02fc82ce
Natalia Misailova <natalia_misailova@epam.com>
2025-08-06 18:30:12 +0300

    add task

---
+++ js/task.js
@@ -0,0 +1,82 @@
+// const age = 10;
+// let total = 150.35;
+
+// total = 200.4;
+
+// const userName = "Nata";
+// const isOpen = true;
+// let user = null;
+// let product;
+
+
+// const x = 10;
+// const y = 3;
+
+// console.log(x ** y);
+
+// const apples = 10;
+// const grepes = 30;
+
+// const total = apples + grepes;
+
+// console.log(total);
+// console.log(apples - grepes);
+
+// let students = 100;
+// // students = students + 50;
+// students += 50;
+
+// console.log(students)
+
+// 
+
+// const companyName = "Boston";
+// const repairBots = 150;
+// const defensBots = 50;
+
+// const total = repairBots + defensBots;
+
+// const massage = `${companyName} has ${total} bots in stock`;
+
+// console.log(massage)
+
+// console.log(String (5));// 5
+// console.log(String (true));// true
+// console.log(String (null));//null
+// console.log(String (undefined));//undefined
+
+// console.log("5" + 3);// 53
+// console.log(5 + "3");// 53
+// console.log(true + "3");// true3
+// console.log(null + "3");// null3
+// console.log("5" + undefined);//5undefined
+// console.log(100 + 2 + "5");// 102+"5"=1025
+
+// console.log(Number ("5"));// 5
+// //Number можно замінити на +
+// console.log(+ ("5"));// 5
+// console.log(+ (true));// 1
+// console.log(Number (false));// 0
+// console.log(Number (null));// 0
+// console.log(Number (undefined));// NaN
+// console.log(Number ("Student"));// NaN
+// console.log(Number ("25Student"));// NaN
+
+// console.log("5" * 2);// 10
+// console.log("5" - 2);// 3
+// console.log("5" / 2);// 2.5
+// console.log(5 + true);// 6
+// console.log(5 + false);// 5
+// console.log(false + true);// 1
+// console.log(5 + true * 5);// 10
+// console.log(10 / (5 * true));// 2
+
+console.log(5>4);//true
+console.log(5<4);//false
+console.log(5>=4);//true
+console.log(5<=4);//false
+console.log(5<="4");//false
+console.log("5">=4);// true
+console.log("5"<="4");//false
+console.log("5">="4");//true
+console.log("2">="12")//true
